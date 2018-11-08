a06e3fdb3628f582960bd2904558cc5bb5880e90
전형탁 <andy395023@gmail.com>
2018-11-08 09:39:36 +0900

    view

--- app.js
+++ app.js
@@ -33,8 +33,6 @@ mongoose.connect('mongodb://localhost/schoolapp',{ useNewUrlParser: true })
 mongoose.Promise = global.Promise; 
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
 
 var db = mongoose.connection;
 db.on('error', console.error);
