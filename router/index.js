85aee4f6ae399a6c032b73660386ec2299bd618b
전형탁 <andy395023@gmail.com>
2018-11-08 10:26:42 +0900

    로그인

--- router/index.js
+++ router/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var passport = require('../config/passport');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    // 로그인 체크
+    if(req.isAuthenticated()){
+        res.render('index');
+    }else{
+        res.render('login');
+    }
 });
 
 module.exports = router;
