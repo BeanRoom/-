00951cf7c5aefb60f959821d6670315970d64191
전형탁 <andy395023@gmail.com>
2018-11-08 20:18:35 +0900

    로그인, 회원가입

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var passport = require('../config/passport');
+var passport = require('passport');
 
 router.get('/', function (req, res, next) {
     // 로그인 체크
@@ -11,5 +11,27 @@ router.get('/', function (req, res, next) {
         res.render('login');
     }
 });
+
+router.get('/signup', function(req,res){
+    res.render('signup');
+});
+
+router.post('/signup', passport.authenticate('signup', {
+    successRedirect : '/', 
+    failureRedirect : '/signup', //가입 실패시 redirect할 url주소
+    failureFlash : true 
+}));
+
+router.post('/login', passport.authenticate('login', {
+    successRedirect : '/', 
+    failureRedirect : '/login', //로그인 실패시 redirect할 url주소
+    failureFlash : true 
+}))
+
+router.get('/logout',function(req,res){
+    req.logout();
+    res.redirect('/');
+});
+
 
 module.exports = router;
